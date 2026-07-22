4b53b7c207aa612cbd1482dad548d1a8f88ea35d
Anushree <159580007+Anushxee@users.noreply.github.com>
2026-07-23 01:13:19 +0530

    Add files via upload

---
+++ Assign Cookies.py
@@ -0,0 +1,9 @@
+def FindContentChildren(g: list, s: list) -> int:
+    g.sort()
+    s.sort()
+    i=j=0
+    while i<=len(g) and j<= len(s):
+        if s[j]>=g[i]:
+            i+=1
+        j+=1
+    return i
